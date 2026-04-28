93128dbb88a75998065755d6d69cfaca6e825aca
rachit kumar <rachitkumar@rachits-MacBook-Air.local>
2026-04-28 12:55:59 +0530

    added jwt auth in swagger

--- config/swagger.js
+++ config/swagger.js
@@ -1,20 +1,36 @@
 const swaggerJsDoc = require("swagger-jsdoc");
 
 const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Banking API",
-      version: "1.0.0",
-      description: "API documentation for Banking Backend",
-    },
-    servers: [
-      {
-        url: "https://banking-backend-n1y8.onrender.com",
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Banking API",
+        version: "1.0.0",
+        description: "API documentation for Banking Backend",
       },
+      servers: [
+        {
+          url: "https://banking-backend-n1y8.onrender.com",
+        },
     ],
-  },
-  apis: ["./routes/*.js"], // where your routes are
+  
+    components: {
+        securitySchemes: {
+          bearerAuth: {
+            type: "http",
+            scheme: "bearer",
+            bearerFormat: "JWT",
+          },
+        },
+      },
+  
+      security: [
+        {
+          bearerAuth: [],
+        },
+      ],
+    },
+    apis: ["./routes/*.js"],
 };
 
 const swaggerSpec = swaggerJsDoc(options);
